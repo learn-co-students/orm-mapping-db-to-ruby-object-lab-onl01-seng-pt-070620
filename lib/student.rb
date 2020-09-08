5039dd8118792b5e206436fcac77ca11bc01b51b
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-09-08 07:48:02 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,87 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+   sql = <<-SQL
+      SELECT * 
+      FROM students 
+   SQL
+
+   mapping_rows_to_objects(sql)
+
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+   sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+   SQL
+
+   mapping_rows_to_objects(sql,name).first
+
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade = '9'
+    SQL
+
+    mapping_rows_to_objects(sql)
+
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade < 12
+    SQL
+
+    mapping_rows_to_objects(sql)
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade = '10'
+      LIMIT ?
+    SQL
+
+    mapping_rows_to_objects(sql,x)
+
+  end
+
+  def self.first_student_in_grade_10
+    first_X_students_in_grade_10(1).first
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    mapping_rows_to_objects(sql,x)
+   
+  end
+
+  def self.mapping_rows_to_objects(sql,x = nil)
+    DB[:conn].execute(sql,x).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def save
