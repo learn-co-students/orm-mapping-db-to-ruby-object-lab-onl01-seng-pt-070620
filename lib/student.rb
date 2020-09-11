798bbe8cfc32ceb4e6af292cab3a5bbd51278ab9
kam3r0nz <kam.hungerford@gmail.com>
2020-09-11 14:10:22 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -75,7 +75,7 @@ class Student
     WHERE grade = 10
     LIMIT ?;
     SQL
-    DB[:conn].execute(sql, x).map {|row| self.new_from_db(row)}
+    DB[:conn].execute(sql, x)
   end
 
   def self.first_student_in_grade_10
