f90195e6a0daa325c9823362add6a96922c12722
github username <github email address>
2020-09-10 20:44:46 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -17,8 +17,9 @@ class Student
     SELECT * 
     FROM students
     SQL
-
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
@@ -33,8 +34,6 @@ class Student
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
-  end
-
   end
   
   def save
@@ -62,4 +61,44 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE students.grade = 9
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end 
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE students.grade < 12
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end 
+  end 
+
+  def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE students.grade = 10
+    ORDER BY students.id 
+    LIMIT ?
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end	  
+
+
+
+
+
 end
