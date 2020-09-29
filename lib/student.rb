aa9577d06b4cd67541bfffe405a71e05249fbd6a
Cailie.Skelton <CSkelton707@gmail.com>
2020-09-29 16:45:15 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
   end
   
    def self.all_students_in_grade_9
-     sql = <<-SQL
+    sql = <<-SQL
     SELECT * 
     FROM students
     WHERE students.grade = 9
@@ -41,20 +41,32 @@ class Student
       self.new_from_db(row)
     end
    end
+
+   def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE students.grade = 10
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end.first
+  end
   
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
     SQL
-
     DB[:conn].execute(sql, self.name, self.grade)
+    
   end
   
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
-      FROM songs
+      FROM students
       WHERE name = ?
       LIMIT 1
     SQL
